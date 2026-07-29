eb3abff2b2c5d141e6d62be5d2e3e05c03d0d123
SebastianKrupinski <krupinskis05@gmail.com>
2026-07-29 09:18:52 -0400

    fix: try to use current-user-privilege-set for permissions
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

--- tests/php/unit/Service/Remote/RemoteConvertTest.php
+++ tests/php/unit/Service/Remote/RemoteConvertTest.php
@@ -54,4 +54,84 @@ class RemoteConvertTest extends TestCase {
 		$this->assertSame(['VEVENT'], RemoteConvert::extractSupportedComponents($value));
 	}
 
+	private function buildAce(string $principalHref, array $privilegeNames): array {
+		return [
+			'name' => '{DAV:}ace',
+			'value' => [
+				[
+					'name' => '{DAV:}principal',
+					'value' => [
+						['name' => RemoteClient::DAV_HREF, 'value' => $principalHref, 'attributes' => []],
+					],
+					'attributes' => [],
+				],
+				[
+					'name' => '{DAV:}grant',
+					'value' => array_map(
+						static fn (string $name) => [
+							'name' => '{DAV:}privilege',
+							'value' => [
+								['name' => $name, 'value' => null, 'attributes' => []],
+							],
+							'attributes' => [],
+						],
+						$privilegeNames,
+					),
+					'attributes' => [],
+				],
+			],
+			'attributes' => [],
+		];
+	}
+
+	public function testExtractPermissionsReturnsPrivilegesKeyedByPrincipal(): void {
+		$acl = [
+			$this->buildAce('/principals/users/3', ['{DAV:}read', '{DAV:}write']),
+			$this->buildAce('/principals/users/55', ['{DAV:}read']),
+		];
+
+		$this->assertSame(
+			[
+				'/principals/users/3' => ['{DAV:}read', '{DAV:}write'],
+				'/principals/users/55' => ['{DAV:}read'],
+			],
+			RemoteConvert::extractPermissions($acl),
+		);
+	}
+
+	public function testExtractPermissionsNormalizesAbsolutePrincipalHrefToPath(): void {
+		$acl = [
+			$this->buildAce('https://dav.example.org/principals/users/3', ['{DAV:}read']),
+		];
+
+		$this->assertSame(
+			['/principals/users/3' => ['{DAV:}read']],
+			RemoteConvert::extractPermissions($acl),
+		);
+	}
+
+	public function testExtractPermissionsIgnoresMalformedEntries(): void {
+		$acl = [
+			'not-an-array',
+			['name' => '{DAV:}ace', 'value' => 'not-an-array', 'attributes' => []],
+			['name' => '{DAV:}other', 'value' => [], 'attributes' => []],
+		];
+
+		$this->assertSame([], RemoteConvert::extractPermissions($acl));
+	}
+
+	public function testExtractPrivilegeNamesParsesCurrentUserPrivilegeSetShape(): void {
+		// {DAV:}current-user-privilege-set carries a flat list of {DAV:}privilege
+		// elements, the same shape as a {DAV:}grant element inside an {DAV:}ace
+		$currentUserPrivilegeSet = [
+			['name' => '{DAV:}privilege', 'value' => [['name' => '{DAV:}read', 'value' => null, 'attributes' => []]], 'attributes' => []],
+			['name' => '{DAV:}privilege', 'value' => [['name' => '{DAV:}write', 'value' => null, 'attributes' => []]], 'attributes' => []],
+		];
+
+		$this->assertSame(
+			['{DAV:}read', '{DAV:}write'],
+			RemoteConvert::extractPrivilegeNames($currentUserPrivilegeSet),
+		);
+	}
+
 }
